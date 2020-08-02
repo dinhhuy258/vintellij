06f34aa05f0711f30d8b194e0fd82b9ae08461a0
Huy Duong <huy.duongdinh@gmail.com>
2020-08-02 13:51:25 +0700

    Kotlin suggest class name for new instance

--- src/main/kotlin/com/dinhhuy258/vintellij/idea/completions/kotlin/ClassNameCompletion.kt
+++ src/main/kotlin/com/dinhhuy258/vintellij/idea/completions/kotlin/ClassNameCompletion.kt
@@ -1,22 +1,57 @@
 package com.dinhhuy258.vintellij.idea.completions.kotlin
 
+import com.dinhhuy258.vintellij.idea.IdeaUtils
 import com.dinhhuy258.vintellij.idea.completions.CompletionKind
 import com.intellij.openapi.project.DumbService
 import com.intellij.psi.CommonClassNames
 import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.search.AllClassesSearchExecutor
 import com.intellij.psi.search.PsiShortNamesCache
+import com.intellij.psi.search.searches.ClassInheritorsSearch
 import com.intellij.psi.search.searches.DirectClassInheritorsSearch
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.idea.caches.resolve.getResolutionFacade
 import org.jetbrains.kotlin.idea.core.KotlinIndicesHelper
+import org.jetbrains.kotlin.idea.refactoring.fqName.fqName
 import org.jetbrains.kotlin.idea.refactoring.fqName.getKotlinFqName
+import org.jetbrains.kotlin.idea.search.declarationsSearch.isInheritable
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
+import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlin.types.isError
 
 class ClassNameCompletion(private val onSuggest: (word: String, kind: CompletionKind, menu: String) -> Unit) {
+    fun findAllClassesMatchType(context: PsiElement, type: KotlinType, prefix: String) {
+        if (type.isError) {
+            findAllClasses(context, prefix)
+            return
+        }
+        val scope = context.resolveScope
+        val typeFqName = type.fqName?.asString() ?: return
+        val psiClass = JavaPsiFacade.getInstance(IdeaUtils.getProject()).findClass(typeFqName, scope) ?: return
+
+        val addToSuggestList: (psiClass: PsiClass) -> Unit = {
+            val className = it.name
+            val classFqName = it.getKotlinFqName()?.asString()
+            if (className != null && classFqName != null && className.startsWith(prefix)) {
+                onSuggest(className, CompletionKind.TYPE, classFqName)
+            }
+        }
+
+        addToSuggestList(psiClass)
+
+        // Find all inheritors
+        if (psiClass.isInheritable()) {
+            val subClasses = ClassInheritorsSearch.search(psiClass, scope, true, true, true)
+            subClasses.forEach {
+                addToSuggestList(it)
+            }
+        }
+    }
+
     fun findAllClasses(context: PsiElement, prefix: String) {
         val scope = context.resolveScope
         val project = context.project
--- src/main/kotlin/com/dinhhuy258/vintellij/idea/completions/kotlin/KtCompletion.kt
+++ src/main/kotlin/com/dinhhuy258/vintellij/idea/completions/kotlin/KtCompletion.kt
@@ -1,13 +1,23 @@
 package com.dinhhuy258.vintellij.idea.completions.kotlin
 
+import com.dinhhuy258.vintellij.idea.IdeaUtils
 import com.dinhhuy258.vintellij.idea.completions.AbstractCompletion
 import com.dinhhuy258.vintellij.idea.completions.CompletionKind
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import org.jetbrains.kotlin.idea.caches.resolve.getResolutionFacade
+import org.jetbrains.kotlin.idea.completion.CompletionBindingContextProvider
+import org.jetbrains.kotlin.idea.references.AbstractKtReference
+import org.jetbrains.kotlin.idea.references.mainReference
 import org.jetbrains.kotlin.idea.util.CallTypeAndReceiver
-import org.jetbrains.kotlin.psi.KtAnnotationEntry
+import org.jetbrains.kotlin.nj2k.postProcessing.type
+import org.jetbrains.kotlin.psi.KtBinaryExpression
+import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.KtSimpleNameExpression
+import org.jetbrains.kotlin.resolve.calls.callUtil.getType
 
 class KtCompletion(onSuggest: (word: String, kind: CompletionKind, menu: String) -> Unit) : AbstractCompletion(onSuggest) {
     override fun doCompletion(psiFile: PsiFile, offset: Int, prefix: String) {
@@ -15,19 +25,52 @@ class KtCompletion(onSuggest: (word: String, kind: CompletionKind, menu: String)
         val completionElement = ktFile.findElementAt(offset)?.parent ?: return
         val application = ApplicationManager.getApplication()
 
-        if (completionElement is KtAnnotationEntry ||
-                (completionElement is KtSimpleNameExpression &&
-                        CallTypeAndReceiver.detect(completionElement) is CallTypeAndReceiver.ANNOTATION)) {
+        val callTypeAndReceiver = getCallTypeAndReceiver(completionElement)
+
+        if (callTypeAndReceiver is CallTypeAndReceiver.ANNOTATION) {
             val classNameCompletion = ClassNameCompletion(onSuggest)
             application.runReadAction {
                 classNameCompletion.findAllAnnotationClasses(completionElement, prefix)
             }
-        } else if (completionElement is KtSimpleNameExpression &&
-                CallTypeAndReceiver.detect(completionElement) is CallTypeAndReceiver.TYPE) {
+        }
+        else if (callTypeAndReceiver is CallTypeAndReceiver.TYPE) {
             val classNameCompletion = ClassNameCompletion(onSuggest)
             application.runReadAction {
                 classNameCompletion.findAllClasses(completionElement, prefix)
             }
         }
+        else if (completionElement is KtElement) {
+            val reference = completionElement.mainReference
+            val bindingContext =
+                    CompletionBindingContextProvider.getInstance(IdeaUtils.getProject())
+                            .getBindingContext(completionElement, completionElement.containingKtFile.getResolutionFacade())
+            if (reference != null && reference is AbstractKtReference<*>) {
+                val expression = reference.expression
+                val parent = expression.parent
+                if (parent is KtBinaryExpression) {
+                    val leftExpression = parent.left ?: return
+                    val type = leftExpression.getType(bindingContext) ?: return
+                    val classNameCompletion = ClassNameCompletion(onSuggest)
+                    application.runReadAction {
+                        classNameCompletion.findAllClassesMatchType(completionElement, type, prefix)
+                    }
+                }
+                else if (parent is KtProperty) {
+                    val type = parent.type() ?: return
+                    val classNameCompletion = ClassNameCompletion(onSuggest)
+                    application.runReadAction {
+                        classNameCompletion.findAllClassesMatchType(completionElement, type, prefix)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getCallTypeAndReceiver(context: PsiElement): CallTypeAndReceiver<*, *>? {
+        if (context is KtSimpleNameExpression) {
+            return CallTypeAndReceiver.detect(context)
+        }
+
+        return null
     }
 }
