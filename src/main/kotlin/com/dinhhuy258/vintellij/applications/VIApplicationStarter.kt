388ed46b8bb8efd596b6eab592c4a7177ec6b748
Huy Duong <huy.duongdinh@gmail.com>
2020-08-09 23:56:51 +0700

    Add vintellij application starter to allow run intellij in headless mode

---
+++ src/main/kotlin/com/dinhhuy258/vintellij/applications/VIApplicationStarter.kt
@@ -0,0 +1,53 @@
+package com.dinhhuy258.vintellij.applications
+
+import com.intellij.ide.impl.ProjectUtil
+import com.intellij.openapi.application.ApplicationStarter
+import com.intellij.openapi.application.ex.ApplicationManagerEx
+import com.intellij.openapi.project.Project
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.system.exitProcess
+
+class VIApplicationStarter : ApplicationStarter {
+    private lateinit var projectPath: String
+
+    override fun getCommandName(): String = "vintellij"
+
+    override fun premain(args: MutableList<String>) {
+        if (args.size == 1 /* First argument is the command name */) {
+            println("Please provide the project path")
+            exitProcess(1)
+        }
+
+        projectPath = args[1] // Ignore the first argument since it's command name
+        val path = try {
+            Paths.get(projectPath)
+        } catch (e: Throwable) {
+            println("Please provide valid project path")
+            exitProcess(1)
+        }
+
+        if (!Files.isDirectory(path)) {
+            println("Please provide valid project path")
+            exitProcess(1)
+        }
+    }
+
+    override fun main(args: MutableList<String>) {
+        openProject(projectPath)
+    }
+
+    private fun openProject(projectPath: String) {
+        println("Opening project...")
+        println("Project path: $projectPath")
+
+        ApplicationManagerEx.getApplicationEx().isSaveAllowed = false
+        val project: Project? = ProjectUtil.openOrImport(projectPath, null, false)
+        if (project == null) {
+            println("Couldn't load project from path: $projectPath")
+            exitProcess(1)
+        }
+
+        println("Project is loaded!!!")
+    }
+}
