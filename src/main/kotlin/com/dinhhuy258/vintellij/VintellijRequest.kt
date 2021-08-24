c4a4579f5abe9ff6d235c3636bc9df463ab448f3
Huy Duong <huy.duongdinh@gmail.com>
2021-08-24 18:44:17 +0700

    Remove Neovim client

--- src/main/kotlin/com/dinhhuy258/vintellij/VintellijRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.dinhhuy258.vintellij
-
-import com.google.gson.JsonElement
-
-data class VintellijRequest(val handler: String, val data: JsonElement)
--- src/main/kotlin/com/dinhhuy258/vintellij/comrade/buffer/Synchronizer.kt
+++ src/main/kotlin/com/dinhhuy258/vintellij/comrade/buffer/Synchronizer.kt
@@ -30,7 +30,6 @@ class Synchronizer(private val syncBuffer: SyncBuffer, var isEnableSync: Boolean
         private set
     private val pendingChanges = mutableMapOf<Int, BufferChange>()
     private val nvimInstance = syncBuffer.nvimInstance
-    private val client = nvimInstance.client
 
     private var changeBuilder: BufferChange.JetBrainsChangeBuilder? = null
     private var changedByNvim = false
@@ -57,27 +56,6 @@ class Synchronizer(private val syncBuffer: SyncBuffer, var isEnableSync: Boolean
     }
 
     fun initFromJetBrain() {
-        val bufId = syncBuffer.id
-        val lines = syncBuffer.document.charsSequence.split('\n')
-        ComradeScope.launch {
-            try {
-                val result = client.api.callAtomic(listOf(
-                    FUN_NVIM_CALL_FUNCTION to listOf(FUN_BUFFER_REGISTER, listOf(
-                        bufId,
-                        nvimInstance.apiInfo.channelId,
-                        lines,
-                        if (isEnableSync) "true" else "false"
-                    )),
-                    FUN_NVIM_BUF_ATTACH to listOf(bufId, false, emptyMap<Any, Any>())
-                ))
-                if (result[1] != null) {
-                    val e = java.lang.IllegalArgumentException("Register buffer failed. $result")
-                    handleException(e)
-                }
-            } catch (t: Throwable) {
-                handleException(t)
-            }
-        }
     }
 
     private fun checkNeovimChangedTick(change: BufferChange): Boolean {
@@ -112,99 +90,9 @@ class Synchronizer(private val syncBuffer: SyncBuffer, var isEnableSync: Boolean
      * Public for mock.
      */
     fun onJetBrainChange(change: BufferChange) {
-        val lines = change.lines ?: throw BufferOutOfSyncException(syncBuffer, change.tick)
-        ComradeScope.launch {
-            try {
-                val result = client.api.callAtomic(listOf(
-                        FUN_NVIM_BUF_SET_LINES to
-                                listOf(change.syncBuffer.id, change.firstLine, change.lastLine, true, lines),
-                        FUN_NVIM_BUF_GET_CHANGEDTICK to listOf(change.syncBuffer.id)
-                ))
-                if (result[1] != null) throw IllegalArgumentException(result[1].toString())
-                val newChangedtick = (result[0] as List<*>)[1] as Int
-                if (newChangedtick != change.tick) throw BufferOutOfSyncException(change.syncBuffer, newChangedtick)
-            } catch (t: Throwable) {
-                handleException(t)
-            }
-        }
     }
 
     private fun onNeovimChange(change: BufferChange) {
-        // The changedtick event only or the change is initialized from JetBrain, nothing has been changed
-        if (checkNeovimChangedTick(change)) return
-
-        // Nullability has been checked in above code
-        val lineData = change.lines!!
-
-        val firstLine = change.firstLine
-        val lastLine = change.lastLine
-
-        val stringBuilder = StringBuilder()
-        lineData.forEachIndexed { index, s ->
-            stringBuilder.append(s)
-            if (index < lineData.size - 1) {
-                stringBuilder.append('\n')
-            }
-        }
-        if (lastLine == -1) {
-            syncBuffer.setText(stringBuilder)
-        } else {
-            val curLineCount = syncBuffer.document.lineCount
-            val document = syncBuffer.document
-            // start should include the previous EOL
-            when {
-                // Deletion
-                lineData.isEmpty() -> {
-                    val start = when (firstLine) {
-                        0 -> 0
-                        curLineCount -> document.getLineEndOffset(firstLine - 1)
-                        else -> document.getLineStartOffset(firstLine) - 1
-                    }
-                    val end = when {
-                        lastLine > curLineCount -> 0
-                        lastLine == curLineCount -> document.getLineEndOffset(lastLine - 1)
-                        start == 0 -> document.getLineStartOffset(lastLine)
-                        else -> document.getLineStartOffset(lastLine) - 1
-                    }
-                    syncBuffer.deleteText(start, end)
-                }
-                firstLine == lastLine -> {
-                    val start = when (firstLine) {
-                        0 -> 0
-                        curLineCount -> document.getLineEndOffset(firstLine - 1)
-                        else -> document.getLineStartOffset(firstLine) - 1
-                    }
-                    if (start != 0) {
-                        stringBuilder.insert(0, '\n')
-                    } else {
-                        stringBuilder.append('\n')
-                    }
-                    // Insertion
-                    syncBuffer.insertText(start,
-                            if (firstLine == curLineCount) stringBuilder
-                            else stringBuilder)
-                }
-                else -> {
-                    val start = when (firstLine) {
-                        0 -> 0
-                        curLineCount -> document.getLineEndOffset(firstLine - 1)
-                        else -> document.getLineStartOffset(firstLine) - 1
-                    }
-                    // Replace the whole end line including EOL
-                    val end = when {
-                        lastLine > curLineCount -> 0
-                        lastLine == curLineCount -> document.getLineEndOffset(lastLine - 1)
-                        else -> document.getLineEndOffset(lastLine - 1) + 1
-                    }
-                    if (firstLine != 0) {
-                        stringBuilder.insert(0, '\n')
-                    }
-                    syncBuffer.replaceText(start, end,
-                            if (lastLine >= curLineCount) stringBuilder
-                            else stringBuilder.append('\n'))
-                }
-            }
-        }
     }
 
     override fun beforeDocumentChange(event: DocumentEvent) {
