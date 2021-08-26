ce6d5174ba460582511fe1d2e12b35da63452eb9
Huy Duong <huy.duongdinh@gmail.com>
2021-08-26 18:39:36 +0700

    Add event notification

---
+++ src/main/kotlin/com/dinhhuy258/vintellij/VintellijEventNotification.kt
@@ -0,0 +1,19 @@
+package com.dinhhuy258.vintellij
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import org.eclipse.lsp4j.jsonrpc.json.MessageJsonHandler
+
+enum class VintellijEventType(val value: Int) {
+    CLOSE_CONNECTION(1)
+}
+
+class VintellijEventNotification(
+    @SerializedName("eventType")
+    @Expose
+    private val type: VintellijEventType,
+) {
+    override fun toString(): String {
+        return MessageJsonHandler.toString(this)
+    }
+}
