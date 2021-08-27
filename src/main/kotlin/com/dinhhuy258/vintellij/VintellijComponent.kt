44abb968e82cfa10e83502b9d8aa5c4b4010b7b3
Huy Duong <huy.duongdinh@gmail.com>
2021-08-27 20:24:22 +0700

    Remove unused stuffs

--- src/main/kotlin/com/dinhhuy258/vintellij/VintellijComponent.kt
+++ src/main/kotlin/com/dinhhuy258/vintellij/VintellijComponent.kt
@@ -5,17 +5,14 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.BaseComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.project.ProjectManagerListener
 import com.intellij.openapi.util.Disposer
-import com.intellij.util.messages.MessageBusConnection
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 
 val VintellijScope = VintellijComponent.instance.coroutineScope
 
+//TODO: Remove this class
 @State(name = "vintellij",
     storages = [Storage(file = "\$APP_CONFIG\$/vintellij_settings.xml")])
 class VintellijComponent : BaseComponent, Disposable {
@@ -25,25 +22,14 @@ class VintellijComponent : BaseComponent, Disposable {
         }
     }
 
-    private lateinit var msgBusConnection: MessageBusConnection
     private lateinit var job: Job
     // Retain a reference to make sure the singleton get initialized
     @Suppress("unused")
 
     val coroutineScope by lazy { CoroutineScope(job + Dispatchers.Default) }
 
-    private val projectManagerListener = object : ProjectManagerListener {
-        override fun projectOpened(project: Project) {
-        }
-
-        override fun projectClosing(project: Project) {
-        }
-    }
-
     override fun initComponent() {
         job = Job()
-        msgBusConnection = ApplicationManager.getApplication().messageBus.connect(this)
-        msgBusConnection.subscribe(ProjectManager.TOPIC, projectManagerListener)
     }
 
     override fun disposeComponent() {
--- src/main/kotlin/com/dinhhuy258/vintellij/formatting/DocumentFormatting.kt
+++ src/main/kotlin/com/dinhhuy258/vintellij/formatting/DocumentFormatting.kt
@@ -1,8 +1,9 @@
 package com.dinhhuy258.vintellij.formatting
 
 import com.dinhhuy258.vintellij.buffer.Buffer
+import com.dinhhuy258.vintellij.utils.invokeAndWait
+import com.dinhhuy258.vintellij.utils.runWriteAction
 import com.dinhhuy258.vintellij.utils.toTextRange
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.psi.codeStyle.CodeStyleManager
@@ -21,11 +22,15 @@ fun formatDocument(buffer: Buffer?, range: Range?) {
         // Save before formatting
         FileDocumentManager.getInstance().saveDocument(buffer.document)
 
-        WriteCommandAction.writeCommandAction(buffer.project)
-            .run<Throwable> {
-                CodeStyleManager.getInstance(buffer.project)
-                    .reformatText(buffer.psiFile, startOffset, endOffset)
+        invokeAndWait {
+            runWriteAction {
+                WriteCommandAction.writeCommandAction(buffer.project)
+                    .run<Throwable> {
+                        CodeStyleManager.getInstance(buffer.project)
+                            .reformatText(buffer.psiFile, startOffset, endOffset)
+                    }
             }
+        }
 
         buffer.psiFile.virtualFile.refresh(true, true)
         FileDocumentManager.getInstance().saveDocument(buffer.document)
