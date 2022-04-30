41fe546cdd6b662906705cb56646e02873a3b243
Huy Duong <huy.duong.ext@personio.de>
2022-04-30 15:45:57 +0700

    Only support nvim version > 0.5.1 and move gen_doc under commands folder

--- lua/vintellij/handlers.lua
+++ lua/vintellij/handlers.lua
@@ -3,115 +3,63 @@ local cfg = require("vintellij.config").config
 
 local M = {}
 
+local EventType = {
+  ["CLOSE_CONNECTION"] = 1,
+  ["REQUEST_COMPLETION"] = 2,
+}
+
 local function notification_handler()
-  if vim.fn.has "nvim-0.5.1" > 0 then
-    vim.lsp.handlers["vintellij/notification"] = function(_, params, ctx, _)
-      if params["eventType"] == 1 then
-        -- Close connection
-        vim.lsp.stop_client(ctx.client_id, true)
-      elseif params["eventType"] == 2 then
-        -- Request completion
-        if vim.fn.mode() ~= "i" then
-          return
-        end
-
-        local status_ok, cmp = pcall(require, "cmp")
-        if not status_ok then
-          return
-        end
-
-        cmp.complete()
+  vim.lsp.handlers["vintellij/notification"] = function(_, params, ctx, _)
+    if params["eventType"] == EventType.CLOSE_CONNECTION then
+      -- Close connection
+      vim.lsp.stop_client(ctx.client_id, true)
+    elseif params["eventType"] == EventType.REQUEST_COMPLETION then
+      -- Request completion
+      if vim.fn.mode() ~= "i" then
+        return
       end
-    end
-  else
-    vim.lsp.handlers["vintellij/notification"] = function(_, _, params, client_id, _)
-      if params["eventType"] == 1 then
-        -- Close connection
-        vim.lsp.stop_client(client_id, true)
-      elseif params["eventType"] == 2 then
-        -- Request completion
-        if vim.fn.mode() ~= "i" then
-          return
-        end
 
-        local status_ok, cmp = pcall(require, "cmp")
-        if not status_ok then
-          return
-        end
-
-        cmp.complete()
+      local status_ok, cmp = pcall(require, "cmp")
+      if not status_ok then
+        return
       end
+
+      cmp.complete()
     end
   end
 end
 
 local function sync_buffer_handler()
-  if vim.fn.has "nvim-0.5.1" > 0 then
-    vim.lsp.handlers["vintellij/syncBuffer"] = function(_, params, _, _)
-      local buffers = vim.api.nvim_list_bufs()
+  vim.lsp.handlers["vintellij/syncBuffer"] = function(_, params, _, _)
+    local buffers = vim.api.nvim_list_bufs()
 
-      for _, buffer in ipairs(buffers) do
-        local fullname = vim.api.nvim_buf_get_name(buffer)
-        if fullname == params["path"] then
-          local ft = vim.api.nvim_buf_get_option(buffer, "filetype")
-          if ft == "kotlin" or ft == "java" then
-            if params["replaceText"] then
-              if cfg.debug then
-                utils.info("Syncing buffer...")
-              end
-
-              vim.api.nvim_buf_set_lines(
-                buffer,
-                params["startLine"],
-                params["endLine"],
-                false,
-                params["lines"]
-              )
-            else
-              local indent = utils.repeats(" ", vim.fn.indent(params["startLine"] + 1))
-              local lines = {}
-              for _, line in ipairs(params["lines"]) do
-                table.insert(lines, indent .. line)
-              end
-              vim.fn.appendbufline(buffer, params["startLine"], lines)
+    for _, buffer in ipairs(buffers) do
+      local fullname = vim.api.nvim_buf_get_name(buffer)
+      if fullname == params["path"] then
+        local ft = vim.api.nvim_buf_get_option(buffer, "filetype")
+        if ft == "kotlin" or ft == "java" then
+          if params["replaceText"] then
+            if cfg.debug then
+              utils.info("Syncing buffer...")
             end
-          end
-          break
-        end
-      end
-    end
-  else
-    vim.lsp.handlers["vintellij/syncBuffer"] = function(_, _, params, _, _)
-      local buffers = vim.api.nvim_list_bufs()
 
-      for _, buffer in ipairs(buffers) do
-        local fullname = vim.api.nvim_buf_get_name(buffer)
-        if fullname == params["path"] then
-          local ft = vim.api.nvim_buf_get_option(buffer, "filetype")
-          if ft == "kotlin" or ft == "java" then
-            if params["replaceText"] then
-              if cfg.debug then
-                utils.info("Syncing buffer...")
-              end
-
-              vim.api.nvim_buf_set_lines(
-                buffer,
-                params["startLine"],
-                params["endLine"],
-                false,
-                params["lines"]
-              )
-            else
-              local indent = utils.repeats(" ", vim.fn.indent(params["startLine"] + 1))
-              local lines = {}
-              for _, line in ipairs(params["lines"]) do
-                table.insert(lines, indent .. line)
-              end
-              vim.fn.appendbufline(buffer, params["startLine"], lines)
+            vim.api.nvim_buf_set_lines(
+              buffer,
+              params["startLine"],
+              params["endLine"],
+              false,
+              params["lines"]
+            )
+          else
+            local indent = utils.repeats(" ", vim.fn.indent(params["startLine"] + 1))
+            local lines = {}
+            for _, line in ipairs(params["lines"]) do
+              table.insert(lines, indent .. line)
             end
+            vim.fn.appendbufline(buffer, params["startLine"], lines)
           end
-          break
         end
+        break
       end
     end
   end
